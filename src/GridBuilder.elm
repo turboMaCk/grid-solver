b3199112e8cba2bd4daec0fc5c56624cc7abcf61
Marek Fajkus <marek.faj@gmail.com>
2016-10-09 17:42:52 +0200

    update to 0.17

--- src/GridBuilder.elm
+++ src/GridBuilder.elm
@@ -1,57 +1,76 @@
-module GridBuilder (GridPosition, isInRange, isTaken, getPosition, getPositions) where
+module GridBuilder exposing (GridPosition, isInRange, isTaken, getPosition, getPositions)
 
 import List
 import Maybe
 
-type alias GridPosition = { x : Int
-                         , y : Int }
+
+type alias GridPosition =
+    { x : Int
+    , y : Int
+    }
+
 
 isInRange : Int -> Int -> Int -> Bool
 isInRange min max value =
-  value >= min && value < max
+    value >= min && value < max
 
-isTaken : List ( GridPosition, { a | height : Int, width : Int } )
-        -> GridPosition
-        -> Bool
+
+isTaken :
+    List ( GridPosition, { a | height : Int, width : Int } )
+    -> GridPosition
+    -> Bool
 isTaken grid position =
-  let
-    conflictX pos item =
-        isInRange pos.x (item.width + pos.x) position.x
-    conflictY pos item =
-        isInRange pos.y (item.height + pos.y) position.y
-  in
-    List.length (List.filter (\(position, item) -> (conflictX position item) && (conflictY position item)) grid) > 0
+    let
+        conflictX pos item =
+            isInRange pos.x (item.width + pos.x) position.x
 
-getPosition : Int
-            -> List ( GridPosition,  { a | width : Int, height : Int } )
-            -> { a | width : Int, height : Int }
-            -> ( GridPosition, { a | width : Int, height : Int } )
+        conflictY pos item =
+            isInRange pos.y (item.height + pos.y) position.y
+    in
+        List.length (List.filter (\( position, item ) -> (conflictX position item) && (conflictY position item)) grid) > 0
+
+
+getPosition :
+    Int
+    -> List ( GridPosition, { a | width : Int, height : Int } )
+    -> { a | width : Int, height : Int }
+    -> ( GridPosition, { a | width : Int, height : Int } )
 getPosition perRow grid item =
-  let
-    last = List.reverse grid |> List.head
+    let
+        last =
+            List.reverse grid |> List.head
 
-    nextPosition position =
-      if position.x + item.width < perRow
-      then { x = position.x + 1, y = position.y }
-      else { x = 0, y = position.y + 1 }
+        nextPosition position =
+            if position.x + item.width < perRow then
+                { x = position.x + 1, y = position.y }
+            else
+                { x = 0, y = position.y + 1 }
 
-    firstTryPosition =
-      case last of
-        Just (lastPosition, lastItem) ->
-          nextPosition { x = lastPosition.x + lastItem.width - 1
-                       , y = lastPosition.y }
-        Nothing -> { x = 0, y = 0 }
+        firstTryPosition =
+            case last of
+                Just ( lastPosition, lastItem ) ->
+                    nextPosition
+                        { x = lastPosition.x + lastItem.width - 1
+                        , y = lastPosition.y
+                        }
 
-    tryPosition position =
-      case isTaken grid position of
-        True -> tryPosition (nextPosition position)
-        False -> position
+                Nothing ->
+                    { x = 0, y = 0 }
 
-  in
-    ( (tryPosition firstTryPosition), item )
+        tryPosition position =
+            case isTaken grid position of
+                True ->
+                    tryPosition (nextPosition position)
 
-getPositions : Int
-             -> List { a | width : Int, height: Int }
-             -> List ( GridPosition, { a | width: Int, height: Int } )
+                False ->
+                    position
+    in
+        ( (tryPosition firstTryPosition), item )
+
+
+getPositions :
+    Int
+    -> List { a | width : Int, height : Int }
+    -> List ( GridPosition, { a | width : Int, height : Int } )
 getPositions perRow list =
-  List.foldl (\item acc -> acc ++ [(getPosition perRow acc item)]) [] list
+    List.foldr (\item acc -> (getPosition perRow acc item) :: acc) [] list
